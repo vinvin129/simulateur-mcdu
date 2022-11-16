c2f049283e8b54b56507f8c09df78a8869411e49
Vincent GUILLOU <vince12juju@gmail.com>
2022-11-16 23:38:17 +0100

    hello-world server

---
+++ src/main/java/fr/ptut2022/simulateurmcdu/mcdu/McduApplication.java
@@ -0,0 +1,23 @@
+package fr.ptut2022.simulateurmcdu.mcdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+@Controller
+public class McduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McduApplication.class, args);
+	}
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+}
---
+++ src/test/java/fr/ptut2022/simulateurmcdu/mcdu/McduApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ptut2022.simulateurmcdu.mcdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
